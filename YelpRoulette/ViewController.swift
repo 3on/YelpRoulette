ae78e26ffb441f5216f6034b3123d2b8fd48659f
bryceAebi <bryce_aebi@brown.edu>
2014-11-06 11:41:04 -0800

    Initial Commit

---
+++ YelpRoulette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpRoulette
+//
+//  Created by Bryce Aebi on 11/6/14.
+//  Copyright (c) 2014 Bryce Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
