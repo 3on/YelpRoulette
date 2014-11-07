fce3ca9cb57f87bc3b4c02c94349aaf8cac60dd1
Jean-Romain Prevost <jrp@140-sfoengwifi54-61.clients.corp.yelpcorp.com>
2014-11-06 16:11:04 -0800

    Add basic interaction with Yelp App

--- YelpRoulette/ViewController.swift
+++ YelpRoulette/ViewController.swift
@@ -13,13 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if self.isYelpAppInstalled() {
+            println("Yelp App found")
+            self.openYelpSearch("sushi")
+        } else {
+            println("Yelp App not found")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func isYelpAppInstalled() -> Bool {
+        let scheme = "yelp:"
+        let url:NSURL = NSURL(string: scheme)!
+        return UIApplication.sharedApplication().canOpenURL(url)
+    }
 
+    private func openYelpSearch(keyword:String) {
+        let queryString = "yelp:///search?term=" + keyword
+        var url:NSURL = NSURL(string:queryString)!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    private func openYelpSentinel() {
+        UIApplication.sharedApplication().openURL(NSURL(string: "yelp:///biz/the-sentinel-san-francisco")!)
+    }
 
 }
 
